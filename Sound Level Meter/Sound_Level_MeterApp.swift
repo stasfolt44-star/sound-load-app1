42345294d4fa7e3b758b772ef5f0e5a178284710
Sergeynsk63 <sergey.nsk63@gmail.com>
2025-12-09 17:58:11 +0400

    Initial Commit

---
+++ Sound Level Meter/Sound_Level_MeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_Level_MeterApp.swift
+//  Sound Level Meter
+//
+//  Created by Sergey on 09.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Sound_Level_MeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
